6efb9bb662c8c0d52f5003bf5f8876378fb9a407
nicolaswiedel <nicolaswiedel@googlemail.com>
2019-03-26 18:00:21 +0100

    gesamtes Projekt

--- core/src/com/tiledmapdemoe/TiledMapDemo.java
+++ core/src/com/tiledmapdemoe/TiledMapDemo.java
@@ -1,23 +1,33 @@
 package com.tiledmapdemoe;
 
 import com.badlogic.gdx.Game;
-import com.tiledmapdemoe.screens.PlayScreen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.tiledmapdemoe.screens.Play;
 
 public class TiledMapDemo extends Game {
 
 	@Override
 	public void create() {
-		setScreen(new PlayScreen());
-	}
-
-	@Override
-	public void render() {
-		super.render();
+		setScreen(new Play());
 	}
 
 	@Override
 	public void dispose() {
 		super.dispose();
+	}
+
+	@Override
+	public void render() {
+		super.render();
+		if(Gdx.input.isKeyPressed(Input.Keys.R))
+			try {
+				setScreen(getScreen().getClass().newInstance());
+			} catch(InstantiationException e) {
+				e.printStackTrace();
+			} catch(IllegalAccessException e) {
+				e.printStackTrace();
+			}
 	}
 
 	@Override
@@ -34,4 +44,6 @@ public class TiledMapDemo extends Game {
 	public void resume() {
 		super.resume();
 	}
+
+
 }
